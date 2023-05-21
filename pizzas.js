394b6b1113f7c9bdda734d3d0fa6bc747dce733e
Paulo Filho <filho9172@gmail.com>
2023-05-20 22:25:43 -0300

    Consertando o desempenho

--- pizzas.js
+++ pizzas.js
@@ -4,6 +4,5 @@ let pizzaJson = [
     {id:3, name:'Quatro Queijos', img:'images/pizza3.webp', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:4, name:'Americana', img:'images/pizza4.webp', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:5, name:'Sorvete', img:'images/pizza5.webp', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.webp', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.webp', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:6, name:'Chocolate', img:'images/pizza7.webp', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
